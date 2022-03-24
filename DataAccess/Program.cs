7afaf2fc37aac1177b330ac63b1eb0b619a8ac33
M Reza Faisal <m_reza_faisal@hotmail.com>
2022-03-24 21:24:24 +0800

    Update Program.cs in DataAccess project, and add SQL script to SQLScript folder.

--- DataAccess/Program.cs
+++ DataAccess/Program.cs
@@ -1,7 +1,14 @@
-﻿// using DataAccess.MySQL;
+﻿//using DataAccess.MySQL;
 using DataAccess.SQLServer;
 
 var db = new northwindContext();
+
+Customer c = new Customer();
+c.CustomerId = "LC";
+c.CompanyName = "Learning Center";
+db.Customers.Add(c);
+db.SaveChanges();
+
 var query = from s in db.Customers select s;
 
 foreach (var item in query)
